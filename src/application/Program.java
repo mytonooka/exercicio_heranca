d29c47ae1069b6a31edd57e6f3e0ce78463e7ab9
Mauricio Yukio Tonooka <160150940+mytonooka@users.noreply.github.com>
2024-02-29 14:23:38 -0300

    Versão 1 - exercicio heranca e polimorfismo

---
+++ src/application/Program.java
@@ -0,0 +1,73 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.Employee;
+import Entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.print("Enter the number of employees: ");
+		int n = sc.nextInt();
+		System.out.println();
+
+		for (int i = 1; i <= n; i++) { 
+			
+			System.out.println("Employee #" + i + " data: ");
+			
+			System.out.print("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			
+			if (ch == 'y' ){
+				System.out.print("Aditional charge: ");
+				double additionalCharge = sc.nextDouble();
+				//Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				//list.add(emp);
+				list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+				
+			}
+			else {
+				
+			//Employee emp = new Employee(name, hours,valuePerHour );
+			//list.add(emp);
+				list.add(new Employee(name, hours, valuePerHour));
+		
+			}
+
+		}
+		
+		System.out.println();
+		
+		System.out.println("PAYMENTS:");
+		
+		for (Employee emp : list) {
+			
+			System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+			
+		}
+
+		sc.close();
+
+	}
+
+}
